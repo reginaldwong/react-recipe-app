b2600e1e25e0b96ab3671ff7382fcdecb26a3bb9
Reginald Wong <reginald_wong@outlook.com>
2021-11-03 18:16:41 -0400

    Edit README.md and App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = "ea378905";
+  const APP_KEY = "78d3c0ecbfdf7ddd4abeceed7e935b44";
+
+  const exampleRequest = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+          <input className="search-bar" type="text"/>
+          <button className="search-button" type="submit">
+            Search
+          </button>
+      </form>
     </div>
   );
 }
