6b0195615ac7dd1f125290381f76db293af01f6d
Reginald Wong <reginald_wong@outlook.com>
2021-11-11 16:34:00 -0500

    edit styling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("chicken");
+  const [query, setQuery] = useState("random");
 
   useEffect(() => {
     getRecipes();
@@ -19,7 +19,6 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits);
   };
 
   const updateSearch = e => {
@@ -29,11 +28,12 @@ const App = () => {
   const getSearch = e => {
     e.preventDefault(); 
     setQuery(search);
-    setSearch('');
+    setSearch('')
   };
 
   return (
     <div className="App">
+      <h1 className='title'>RecipeFinder</h1>
       <form onSubmit={getSearch} className="search-form">
           <input 
           className="search-bar" 
@@ -49,7 +49,6 @@ const App = () => {
       <div className="recipes">
       {recipes.map(recipe => (
         <Recipe 
-        key={recipe.recipe.label}
         title={recipe.recipe.label} 
         calories={recipe.recipe.calories}
         image={recipe.recipe.image}
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import style from './recipe.module.css'
+import style from './recipe.module.css';
 
 const Recipe = ({title, calories, image, directions, ingredients}) => {
 
     return (
         <div className={style.recipe}>
             <h1>{title}</h1>
-            <img className={style.image} src={image} alt="" />  
+            <img className={style.recipe} src={image} alt="" />  
             <ol>
                 {ingredients.map(ingredient => (
                     <li>{ingredient.text}</li>
                 ))}
             </ol>
-             <p>{calories}</p>
-             <a className={style.link} href={directions} target="_blank" rel="noreferrer">Recipe</a>
-           
-             
+             <a href={directions} target="_blank" rel="noreferrer">Recipe</a>
         </div>
     );
 }
